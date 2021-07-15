dd6242492c8d9f69a392ae6a988a6212aadd4d1a
celerygit <celery1798@163.com>
2021-07-15 16:23:31 +0800

    new__mod_

---
+++ c/basic/op.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int i;
+	float f = 3.9;
+
+	i = (int)f;
+
+	printf("i = %d\n",i);
+	printf("f = %f\n",f);
+
+
+
+
+
+	/*
+	int a=1,b =2,c=3,d=4,m=1,n=1;
+
+	(m = a > b) && (n = c > d);
+	
+	printf("m = %d, n = %d\n",m,n);
+*/
+
+	exit(0);
+}
+
