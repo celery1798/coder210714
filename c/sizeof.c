5982b6ffe93611c8eb8d1a8fe2a8197885b2b7c5
celerygit <celery1798@163.com>
2021-07-15 10:36:04 +0800

    new__mod_

---
+++ c/sizeof.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int i = 23;
+
+	printf("sizeof(int) = %ld\n",sizeof(i));
+	printf("sizeof(double) = %ld\n",sizeof(double));
+
+
+	exit(0);
+}
+
