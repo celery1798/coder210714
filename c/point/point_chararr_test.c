ed04a2bb3d94988773be59e82261693b2bae05f4
celerygit <celery1798@163.com>
2021-07-23 14:24:03 +0800

    new__mod_

--- c/point/point_chararr_test.c
+++ c/point/point_chararr_test.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 int main()
 {
 	char *name[] = {"Follow Me","BASIC","Great Wall","Computer","Python"};
-	int i;
+	int i,j,k;
+	char *tmp = NULL;	// void *tmp = NULL;
 
 	//sort
-	
-
-
-
-
-
-
+	for(i = 0 ; i < 4; i++)
+	{
+		k = i;
+		for(j = i+1; j < 5;j++)
+		{
+			if(strcmp(name[k],name[j]) > 0)
+				k = j;
+		}
+		if(k != i)
+		{
+			tmp = name[i];
+			name[i] = name[k];
+			name[k] = tmp;
+		}
+	}
 
 	for(i = 0 ; i < 5; i++)
 		puts(name[i]);
